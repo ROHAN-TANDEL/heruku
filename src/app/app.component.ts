c0d1a88da6b2edeb3447801221bfe6a27b6b6b37
rohan <rohan.t@scriptbees.com>
2018-10-08 16:57:08 +0530

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { LoginService } from './service';
+//import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'heruko-test';
+  title = 'auro';
+  constructor(private loginService: LoginService) {}
+  getdata() {
+    alert('clcik');
+    this.loginService.login();
+  }
 }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private _router: Router,) { }
+role="researcher"
+  ngOnInit() {
+  }
+  login(email, password){
+   
+
+  	if(this.role=="admin"){
+  		this._router.navigate(['/app/admin']);
+  	}
+    else if(this.role=="researcher"){
+      console.log("here")
+     this._router.navigate(['/app/researcher']);
+    }
+  	else{
+  	    this._router.navigate(['/app/user']);	
+  	}
+  	
+  }
+
+}
